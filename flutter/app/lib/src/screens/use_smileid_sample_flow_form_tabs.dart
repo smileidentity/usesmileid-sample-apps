de91fa542837e43aa5c833bbc645ddcbeb9755e8
Harun Wangereka <harunwangereka@gmail.com>
2026-09-23 02:30:33 +0300

    chore: drop the porting-status comment from the Flutter ID form host

--- flutter/app/lib/src/screens/use_smileid_sample_flow_form_tabs.dart
+++ flutter/app/lib/src/screens/use_smileid_sample_flow_form_tabs.dart
@@ -116,7 +116,6 @@ class _UseSmileIDSampleKycFormTabState
               ? UseSmileIDSampleRoutes.sdkFlow(widget.productId)
               : UseSmileIDSampleJourney.afterIdDetails(product),
         ),
-        // Inert like the nav bar's token button until the scan screen is ported.
         onScanToken: () {},
       ),
     );
